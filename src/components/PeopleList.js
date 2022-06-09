63950bb9875852e13a79adb48c1c311e5fe26a19
DaBren20 <dabrentwenty@gmail.com>
2022-06-09 14:04:10 -0400

    grid centering

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -6,7 +6,7 @@ import { Grid } from 'semantic-ui-react';
 const PeopleList = ( { data } ) => {
   return (
     <div>
-      <Grid columns={3}>
+      <Grid columns={4} centered>
         
           {data.map((people, index) => {return ((<Grid.Column><InfoCard key={index} name={people.name} gender={people.gender} height={people.height} mass={people.mass} birth_year={people.birth_year} eye_color={people.eye_color} /> </Grid.Column>))})}
         
