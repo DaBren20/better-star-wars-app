df681110542199b8092c2031c7a76e6b4c1ae6a1
DaBren20 <dabrentwenty@gmail.com>
2022-06-09 01:19:52 -0400

    Fixed API call for people

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -8,7 +8,7 @@ const PeopleList = ( { data } ) => {
     <div>
           <Grid columns={3}>
             <Grid.Column>
-              {data.map((people, index) => {return ((<InfoCard key={index} name={people.name} gender={people.gender} height={people.height} mass={people.mass} />))})}
+              {data.map((people, index) => {return ((<InfoCard key={index} name={people.name} gender={people.gender} height={people.height} mass={people.mass} birth_year={people.birth_year}/>))})}
             </Grid.Column>
           </Grid>
     </div>
