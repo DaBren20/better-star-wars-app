24a3c832c7878161c1dab7552d77bc24ca7ef1d9
DaBren20 <dabrentwenty@gmail.com>
2022-06-10 01:26:31 -0400

    Adjusting banner

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -7,9 +7,7 @@ const PeopleList = ( { data } ) => {
   return (
     <div>
       <Grid columns={4} centered>
-        
-          {data.map((people, index) => {return ((<Grid.Column><InfoCard key={index} name={people.name} gender={people.gender} height={people.height} mass={people.mass} birth_year={people.birth_year} eye_color={people.eye_color} skin_color={people.skin_color} /> </Grid.Column>))})}
-        
+          {data.map((people, index) => {return ((<Grid.Column><InfoCard key={index} name={people.name} gender={people.gender} height={people.height} mass={people.mass} birth_year={people.birth_year} eye_color={people.eye_color} skin_color={people.skin_color} /> </Grid.Column>))})}  
       </Grid>
     </div>
   )
