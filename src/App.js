76fa6b879ae0a6aab8ea80293b298d5c955194d0
DaBren20 <dabrentwenty@gmail.com>
2022-06-13 23:54:24 -0400

    Added planets fetch request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import React, { useState, useEffect } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -12,23 +11,10 @@ import { Container, Dimmer, Loader } from 'semantic-ui-react';
 import Home from './pages/Home';
 import PeoplePage from './pages/PeoplePage';
 import PlanetPage from './pages/PlanetPage';
-import People from './components/People';
 
 function App() {
-  const [planets, setPlanets] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch(`https://swapi.dev/api/planets/?format=json`)
-    .then(res => res.json())
-    .then(json => {
-      console.log("Planets", json.results)
-      setPlanets(json.results)
-    })
-    .catch(err => {
-      console.log(`Error: ${err}`)
-    })
-  },[])
 
   //setLoading(false);
 
@@ -43,7 +29,7 @@ function App() {
           <Routes>
             <Route path= "/" element={<Home />} />
             <Route path="/people" element={<PeoplePage />} />
-            <Route path="/planets" element={<PlanetPage data={planets} />}/>
+            <Route path="/planets" element={<PlanetPage />}/>
           </Routes>
         {/*)} */}
       </Router>
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import React, { useState, useEffect } from 'react';
+import PlanetsList from './PlanetsList';
 
 const Planets = () => {
+  const [planets, setPlanets] = useState([]);
+  
+  useEffect(() => {
+    fetch(`https://swapi.dev/api/planets/?format=json`)
+    .then(res => res.json())
+    .then(json => {
+      console.log("Planets", json.results)
+      setPlanets(json.results)
+    })
+    .catch(err => {
+      console.log(`Error: ${err}`)
+    })
+  },[])
   return (
-    <div>Planets</div>
+    <div>
+      <PlanetsList data={planets} />
+    </div>
   )
 }
 
