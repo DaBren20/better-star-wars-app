0504e462b4541b11b2505d08d1a3b44f369b36de
DaBren20 <dabrentwenty@gmail.com>
2022-06-23 22:57:18 -0400

    Added Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
         <p>This website uses the Star Wars API (<a href="https://swapi.dev/">SWAPI</a>) to grab data from the API and display the data into cards. This is just a small example of my skill to retrieve data from an API that is not mine.</p>
         <p>Filler</p>
         <p>More Filler</p>
+        <h1>Even more filler</h1>
     </div>
   )
 }
